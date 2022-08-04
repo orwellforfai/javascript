50d1d37f3d83daba48b8a6f4071d9851684359c9
Martin Beltramo <94710842+orwellforfai@users.noreply.github.com>
2022-08-04 20:34:57 -0300

    Version con Array y Objetos

--- js/carrito.js
+++ js/carrito.js
@@ -1,66 +1,67 @@
 'use strict';
 
-// Variables definition
-const percent = 0.1
-let price1 = 3500 //Servicio 1
-let price2 = 5000 //Servicio 2
-let price3 = 5000 //Servicio 3
-let contador1 = 0
-let contador2 = 0
-let contador3 = 0
-let total = 0
-
-
-alert ("Los productos disponibles para adquirir son los siguientes: " +"\nServicio 1 " +"$" + price1 +
-"\nServicio 2 " +"$"+ price2 +
-"\nServicio 3 " +"$"+ price3
-)
-
-// User Choice
-let choice = prompt("Ingrese el servicio que quiere adquirir")
-
-do{
-
-    // Switch de decision y acumulacion
-    switch(choice){
-        case "1":
-            alert("Eligio 1");
-            contador1 = contador1 + 1
-            console.log (contador1)
-            break;
-
-        case "2":
-            alert("Eligio 2");
-            contador2 = contador2 + 1
-            console.log (contador2)
-            break;
-        case "3":
-            alert("Eligio 3");
-            contador3 = contador3 + 1
-            console.log (contador3)
-            break;
-
+//CLASSES
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre
+        this.precio = precio
     }
-    choice = prompt("Ingrese el servicio que quiere adquirir");
-    console.log(choice);
 
-    //Si el parseo no resulta un número se interrumpe el bucle.
-}while(parseInt(choice));
-
-//llamo la funcion para calcular los totales
-calcularTotal()
-
-//funcion de calculo
-function calcularTotal() {
-    total = (price1*contador1) + (price2*contador2) + (price3*contador3)
-    if (total > 10000) {
-        alert(" Se aplica un descuento por que el total es mayor a $10000")
-        total = total - (total * percent)
-        alert("El Total a cobrar es: " + total)
+    mostrarProducto() {
+        console.log("Nombre: " + this.nombre + "\nPrecio: " + this.precio)
     }
-    else
-    {
-        alert ("El Total a cobrar es: " +total + " No hay descuentos por no superar los $10000")
-    }
-console.log(total)
 }
+
+//ARRAY DEFINITION
+const pedidos = []
+
+
+// VARIABLES DEFINITION
+let producto1 = new Producto("Hamburguesa", 400)      //Producto 1
+let producto2 = new Producto("Pizza Muzzarella", 600)  //Producto 2
+let total = 0
+let choice = 0
+
+//FUNCTION DEFINITION
+function productSelection() {
+    choice = prompt("Ingrese los productos que desea adquirir" );
+    do {
+        // SWITCH
+        switch (choice) {
+            case "1":
+                alert("Eligio 1");
+                pedidos.push(producto1)
+                break;
+
+            case "2":
+                alert("Eligio 2");
+                pedidos.push(producto2)
+                break;
+            default:
+                alert("Elegio un producto no valido")
+                break;
+        }
+        choice = prompt("Ingrese los productos que desea adquirir");
+        console.log(choice);
+
+        //Si el parseo no resulta un número se interrumpe el bucle.
+    } while (parseInt(choice));
+}
+
+function totalCalculation() {
+    for (const pedido of pedidos) {
+        total = total + pedido.precio
+    }
+}
+
+function showCalculation() {
+    console.log("El total a abonar por los productos seleccionados es: " + total + " pesos")
+}
+
+
+// PROGRAM
+console.log("Los productos disponibles son: " + producto1.mostrarProducto() + producto2.mostrarProducto())
+productSelection()
+console.log(pedidos)
+totalCalculation()
+showCalculation()
